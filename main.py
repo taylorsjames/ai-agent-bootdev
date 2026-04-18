14f77585b932ffa5aef20f339784efc1a40582d8
taylorsjames <t.james1017@gmail.com>
2026-04-18 03:27:56 -0400

    ch1 complete

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 from dotenv import load_dotenv
 from google import genai
 import argparse
+from google.genai import types
+
 
 #loading stuff we need
 
@@ -16,13 +18,16 @@ if api_key == None:
 #prompt user for input
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
+
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 
 def main():
     
     response = client.models.generate_content(
-    model='gemini-2.5-flash', contents=args.user_prompt
+    model='gemini-2.5-flash', contents=messages
 )
     if response.usage_metadata == None:
         raise RuntimeError("Response empty, please check API key or connection")
@@ -30,11 +35,14 @@ def main():
     prompt_token_count = response.usage_metadata.prompt_token_count
     response_token_count = response.usage_metadata.candidates_token_count
 
-    print(f'User Prompt: {args.user_prompt}')
-    print(f'Prompt tokens: {prompt_token_count}')
-    print(f'Response tokens: {response_token_count}')
-    print(f'Response:\n{response.text}')
-
+    if args.verbose == True:
+        
+        print(f'User prompt: {args.user_prompt}')
+        print(f'Prompt tokens: {prompt_token_count}')
+        print(f'Response tokens: {response_token_count}')
+        
+    if args.verbose == False:
+        print(f'Response:\n{response.text}')
 
 if __name__ == "__main__":
     main()
